15c8920621b1cf2e879a92bf75cb7ad76698f7b1
Brett Andrews <brettandrews3@gmail.com>
2021-09-27 17:20:15 -0500

    Setup sample text file and compute_readability().

---
+++ analyze.py
@@ -0,0 +1,11 @@
+# Analyzes text readability; part of the Readability_Test project.
+
+import ch1text
+
+def compute_readability(text):
+    total_words = 0
+    total_sentences = 0
+    total_syllables = 0
+    score = 0
+
+compute_readability(ch1text.text)
