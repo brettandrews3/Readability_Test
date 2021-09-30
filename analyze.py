f54aa3f45df1e9bab9e688244d0a3801a3b3e7bd
Brett Andrews <brettandrews3@gmail.com>
2021-09-30 17:20:31 -0500

    Added split function to readability test. Next: add count_sentences().

--- analyze.py
+++ analyze.py
@@ -7,5 +7,11 @@ def compute_readability(text):
     total_sentences = 0
     total_syllables = 0
     score = 0
+    
+    words = text.split()
+    total_words = len(words)
+    
+    print(words)
+    print(total_words, 'words')
 
 compute_readability(ch1text.text)
